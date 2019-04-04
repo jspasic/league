c905115f945db13d3eada2310e61b647bdf600f5
Jovan Spasic <jspasic@live.com>
2019-04-04 21:15:40 +0200

    Explicitely enable JPA repositories

--- src/main/java/rs/jspasic/league/LeagueApplication.java
+++ src/main/java/rs/jspasic/league/LeagueApplication.java
@@ -2,8 +2,10 @@ package rs.jspasic.league;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class LeagueApplication {
 
 	public static void main(String[] args) {
