9fbd090cd0fa18e6af988e8c5bf50895f1cb0f1b
Jovan Spasic <jspasic@live.com>
2019-04-04 21:16:59 +0200

    Fix results endpoint handler
    Add an ugly method that stores everything required to store game results

--- src/main/java/rs/jspasic/league/web/api/ResultsController.java
+++ src/main/java/rs/jspasic/league/web/api/ResultsController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import rs.jspasic.league.exception.GroupNotFoundException;
+import rs.jspasic.league.exception.LeagueNotFoundException;
+import rs.jspasic.league.exception.TeamNotFoundException;
 import rs.jspasic.league.model.Game;
 import rs.jspasic.league.model.Group;
 import rs.jspasic.league.model.GroupStandings;
@@ -20,7 +23,11 @@ import rs.jspasic.league.service.TeamService;
 import rs.jspasic.league.util.StandingsCalculator;
 import rs.jspasic.league.web.model.GameWrapper;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -43,21 +50,28 @@ public class ResultsController {
 
     @PostMapping(path = "/group")
     public List<GroupStandings> saveGames(@RequestBody List<GameWrapper> gameResults) {
+        validateSaveGameRequest(gameResults);
+
+
+        String leagueName = gameResults.stream()
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Invalid input"))
+                .getLeagueTitle();
+
         List<Game> games = gameResults.stream()
                 .map(this::extractAndHydrateGame)
                 .collect(Collectors.toList());
 
-        games = gameService.saveAllGames(games);
+        gameService.saveAllGames(games);
 
-        Set<Group> groups = games.stream()
-                .map(Game::getGroup)
-                .collect(Collectors.toSet());
+        List<Group> groups = groupService.findByLeagueName(leagueName);
+        for (Group group: groups) {
+            List<Game> groupGames = gameService.findByGroup(group);
+            group.setGames(groupGames);
+        }
 
-        List<GroupStandings> allGroupsStandings = groups.stream()
-                .map(StandingsCalculator::getGroupStandings)
-                .collect(Collectors.toList());
 
-        return allGroupsStandings;
+        return groupService.getGroupsStandings(groups);
     }
 
     @GetMapping("/{leagueId}")
@@ -69,20 +83,90 @@ public class ResultsController {
             groups = groupService.findByLeagueIdAndGroupNames(leagueId, requestedGroups);
         }
 
-        List<GroupStandings> allGroupsStandings = groups.stream()
+        return groups.stream()
                 .map(StandingsCalculator::getGroupStandings)
                 .collect(Collectors.toList());
+    }
 
-        return allGroupsStandings;
+    // makes sure all data is in place for recording game results
+    private void validateSaveGameRequest(List<GameWrapper> gameResults) {
+        if (gameResults.isEmpty()) {
+            throw new RuntimeException("Results list cannot be empty");
+        }
+
+        Set<String> leagueNames = new HashSet<>();
+        Set<String> groupNames = new HashSet<>();
+        Set<String> teamNames = new HashSet<>();
+        Map<String, Set<String>> groupsTeams = new HashMap<>();
+        for (GameWrapper result: gameResults) {
+            leagueNames.add(result.getLeagueTitle());
+            groupNames.add(result.getGroup());
+            teamNames.add(result.getHomeTeam());
+            teamNames.add(result.getAwayTeam());
+
+            if (!groupsTeams.containsKey(result.getGroup())) {
+                Set<String> val = new HashSet<>();
+                groupsTeams.put(result.getGroup(), val);
+            }
+            groupsTeams.get(result.getGroup()).add(result.getHomeTeam());
+            groupsTeams.get(result.getGroup()).add(result.getAwayTeam());
+        }
+
+        if (leagueNames.size() != 1) {
+            throw new RuntimeException("Results can be added for one league only in one request");
+        }
+
+        List<Team> teams = new ArrayList<>();
+        for (String tname: teamNames) {
+            try {
+                Team t = teamService.findTeamByName(tname);
+                teams.add(t);
+            } catch (TeamNotFoundException e) {
+                Team t = new Team(tname);
+                t = teamService.addTeam(t);
+                teams.add(t);
+            }
+        }
+
+        String leagueName = leagueNames.stream().findFirst().get();
+        League league;
+        try {
+            league = leagueService.findLeagueByName(leagueName);
+        } catch (LeagueNotFoundException e) {
+            league = new League(leagueName);
+            league.setTeams(teams);
+            league = leagueService.addLeague(league);
+        }
+
+        List<Group> groups = new ArrayList<>();
+        for (String gname: groupNames) {
+            try {
+                Group g = groupService.findByLeagueNameAndGroupName(leagueName, gname);
+                groups.add(g);
+            } catch (GroupNotFoundException e) {
+                Group g = new Group(league, gname);
+
+                Set<String> tnames = groupsTeams.get(gname);
+                List<Team> groupTeams = tnames.stream()
+                        .map(teamService::findTeamByName)
+                        .collect(Collectors.toList());
+                g.setTeams(groupTeams);
+
+                groupService.addGroup(g);
+            }
+        }
     }
 
     private Game extractAndHydrateGame(GameWrapper gameWrapper) {
-
         League league = leagueService.findLeagueByName(gameWrapper.getLeagueTitle());
         Group group = groupService.findByLeagueNameAndGroupName(gameWrapper.getLeagueTitle(), gameWrapper.getGroup());
         Team homeTeam = teamService.findTeamByName(gameWrapper.getHomeTeam());
         Team awayTeam = teamService.findTeamByName(gameWrapper.getAwayTeam());
 
+        return populateGame(gameWrapper, league, group, homeTeam, awayTeam);
+    }
+
+    private Game populateGame(GameWrapper gameWrapper, League league, Group group, Team homeTeam, Team awayTeam) {
         Game game = gameWrapper.getGame();
         game.setLeague(league);
         game.setGroup(group);
